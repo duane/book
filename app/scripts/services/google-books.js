fb56ef72a5a972845a04447a340668b3609bfc66
Duane Bailey <bailey.d.r@gmail.com>
2013-11-11 17:03:45 -0500

    removed old file.

--- app/scripts/services/google-books.js
+++
@@ -1,5 +0,0 @@
-angular.module('book.google-books', ['ngResource']).
-  factory('googleBooks', ['$resource', function($resource) {
-    return $resource('https://www.googleapis.com/books/v1/volumes?q=isbn\::isbn', {isbn: '@isbn'});
-  }]).
-  value('version', '0.0.1');
