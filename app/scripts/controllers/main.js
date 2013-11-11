3d04bd7134f9d189f6c56be196f967e650f77fe5
Duane Bailey <bailey.d.r@gmail.com>
2013-11-11 16:53:48 -0500

    Book addition works.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,23 +1,33 @@
 'use strict';
 var book = angular.module('book');
 
-var BookCtrl = book.controller('BookCtrl', ['$scope', function($scope) {}]);
+var BookCtrl = book.controller('BookCtrl', angular.noop);
 
-var BookChoiceCtrl = book.controller('BookChoiceCtrl', ['$http', '$scope', '$modal', '$log', 'alertService', function($http, $scope, $modal, $log, alertService) {
-  $scope.AddBook = function() {
+book.controller('BookShelfCtrl', function BookShelfCtrl($http, $scope, $modal, $log, alertService) {
+  $scope.books = [];
+  $scope.FetchISBN = function() {
     $http({
       url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:'.concat(this.isbn),
       method: 'GET'
     }).then(
       // resolve
       function(response) {
-        $log.log(this);
-        $log.log(response.data);
         var result = response.data;
         if ('items' in result) {
+          var childScope = $scope.$new(true);
+          childScope.books = result.items;
           $modal.open({
-            templateUrl: 'views/book_choice.html'
-        });
+            templateUrl: 'views/book_choice.html',
+            controller: BookModalCtrl,
+            scope: childScope
+          }).result.then(
+            function(book) {
+              $scope.books.push(book);
+            },
+            function(reason) {
+              alertService.AddAlert('warning', reason);
+            }
+          );
         } else {
           alertService.AddAlert('warning', "Couldn\'t find the isbn!");
         }
@@ -28,14 +38,14 @@ var BookChoiceCtrl = book.controller('BookChoiceCtrl', ['$http', '$scope', '$mod
       }
     );
   };
-}]);
-
-var BookModalCtrl = book.controller('BookModalCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance) {
-
-}]);
+});
 
 var AlertController = book.controller('AlertController', ['$scope', 'alertService', function($scope, alertService) {
   $scope.CloseAlert = function(index) {
     alertService.CloseAlert(index);
   };
 }]);
+
+function BookModalCtrl($scope, $modalInstance) {
+  $scope.modal = $modalInstance;
+}
